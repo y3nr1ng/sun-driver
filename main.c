b6a1c54ab97d6204c3fb93dab2d5bb1e7ccc508a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-01 13:49:21 +0800

    Add main.c file.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // configure i/o
+
+    // timer initialize.
+    return 0;
+}
---
+++ pinout.h
@@ -0,0 +1,6 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+#define BACKGROUND_LED  
+
+#endif
