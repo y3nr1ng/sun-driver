46c622d1b438e0d90c1add418d1b1cbb14b2ceaa
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-02 17:17:23 +0800

    Implement general plotting functions.

--- lib/glcd.h
+++ lib/glcd.h
@@ -6,24 +6,24 @@ void glcd_init(void);
 void clear_screen(void);
 
 // Plotting functions.
-void setpixel(uint8_t *buff, uint8_t x, uint8_t y, uint8_t color);
+void setpixel(uint8_t *buf, uint8_t x, uint8_t y, uint8_t color);
 
-void drawrect(uint8_t *buff,
+void drawrect(uint8_t *buf,
               uint8_t x, uint8_t y, uint8_t w, uint8_t h,
               uint8_t color);
-void drawline(uint8_t *buff,
+void drawline(uint8_t *buf,
               uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1,
               uint8_t color);
 
-void drawbitmap(uint8_t *buff, uint8_t x, uint8_t y,
+void drawbitmap(uint8_t *buf, uint8_t x, uint8_t y,
                 const uint8_t bitmap, uint8_t w, uint8_t h,
                 uint8_t color);
-void drawchar(uint8_t *buff, uint8_t x, uint8_t line, uint8_t c);
-void drawcircle(uint8_t *buff,
+void drawchar(uint8_t *buf, uint8_t x, uint8_t line, uint8_t c);
+void drawcircle(uint8_t *buf,
                 uint8_t x0, uint8_t y0, uint8_t r,
                 uint8_t color);
 
-void fillrect(uint8_t *buff,
+void fillrect(uint8_t *buf,
               uint8_t x, uint8_t y, uint8_t w, uint8_t h,
               uint8_t color);
 
