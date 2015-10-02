43f0d9b590bbe18d2cbc875c6d22818f6dd0dd72
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-02 17:13:57 +0800

    Add initialization function.

--- lib/driver/st7565.c
+++ lib/driver/st7565.c
@@ -2,7 +2,49 @@
 
 void st7565_init(void)
 {
+	// Set pin directions.
+	#ifdef SPI
+	SID_DDR |= _BV(SID);
+	SCLK_DDR |= _BV(SCLK);
+	#elif
+	DATA_DDR |= _BV(0xFF);
+	#endif
+	A0_DDR |= _BV(A0);
+	RST_DDR |= _BV(RST);
+	CS_DDR |= _BV(CS);
 
+	// Manually initiate the chip selection.
+	CS_PORT &= ~_BV(CS);
+	RST_PORT &= ~_BV(RST);
+	_delay_ms(500);
+	RST_PORT |= _BV(RST);
+
+	// LCD bias select.
+	st7565_command(CMD_SET_BIAS_7);
+	// ADC select.
+	st7565_command(CMD_SET_ADC_NORMAL);
+	// SHL select.
+	st7565_command(CMD_SET_COM_NORMAL);
+	// Initial display line.
+	st7565_command(CMD_SET_DISP_START_LINE);
+
+	// Turn on voltage converter (VC=1, VR=0, VF=0)..
+	st7565_command(CMD_SET_POWER_CONTROL | 0x4);
+	// Wait for 50% rising.
+	_delay_ms(50);
+
+	// turn on voltage regulator (VC=1, VR=1, VF=0).
+	st7565_command(CMD_SET_POWER_CONTROL | 0x6);
+	// Wait >=50ms.
+	_delay_ms(50);
+
+	// Turn on voltage follower (VC=1, VR=1, VF=1).
+	st7565_command(CMD_SET_POWER_CONTROL | 0x7);
+	// Short wait for the command to complete.
+	_delay_ms(10);
+
+	// Set LCD operating voltage (regulator resistor, ref voltage resistor)
+	st7565_command(CMD_SET_RESISTOR_RATIO | 0x6);
 }
 
 void st7565_clear_screen(void)
@@ -21,7 +63,11 @@ void st7565_clear_screen(void)
 	}
 }
 
-void st7565_clear_buffer(uint8_t *buf);
+void st7565_clear_buffer(uint8_t *buf)
+{
+
+}
+
 void st7565_write_buffer(uint8_t *buf)
 {
 	uint8_t c, p;
@@ -35,7 +81,7 @@ void st7565_write_buffer(uint8_t *buf)
 		st7565_data(0xff);
 
 		for (c = 0; c < 128; c++)
-			st7565_data(buffer[(128*p)+c]);
+			st7565_data(buf[(128*p)+c]);
 	}
 }
 
